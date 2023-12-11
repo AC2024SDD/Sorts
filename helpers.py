18e156447f225d3512f8e0be89166cfed5a3496e
Michael Kelly <92009909+mkelly133@users.noreply.github.com>
2023-12-11 14:59:22 +1100

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,15 +1,13 @@
-
-""" Complete each of the sorts! """
-
-def bubble_sort(words):
-    '''Write your code here!'''
-    pass
-
-def selection_sort(words):
-    '''Write your code here!'''
-    pass
-
-def insertion_sort(words):
-    '''Write your code here!'''
-    pass
-
+def load_list(csv_file):
+    try:
+        with open(csv_file, mode='r', newline='') as file:
+            reader = csv.reader(file)
+            # Assuming the 'Word' column is in the first position (index 0)
+            for row in reader:
+                word_list.append(row[0])    
+        print("Words loaded successfully!")
+        print("Words List:", word_list)
+    except FileNotFoundError:
+        print(f"File '{csv_file}' not found.")
+    except Exception as e:
+        print("An error occurred:", str(e))
