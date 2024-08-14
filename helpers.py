72ccb2c4d1aa4a4d4a6895414f75a41012a3df8a
Adam-SDD <124738300+Adam-SDD@users.noreply.github.com>
2024-08-14 14:44:03 +1000

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -2,8 +2,20 @@
 """ Complete each of the sorts! """
 
 def bubble_sort(words):
-    '''Write your code here!'''
-    pass
+    last = len(words)
+    swapped = True
+    while swapped == True:
+        swapped = False
+        i = 1
+        while i < last:
+            if words[i] > words[i+1]:
+                temp = words[i]
+                words[i] = words[i+1]
+                words[i+1] = temp
+                swapped = True
+            i =+ 1
+        last =- 1
+    return words
 
 def selection_sort(words):
     '''Write your code here!'''
